6fc2d053fa5265b5f4cea8ec41e4d9bfd14bb719
Scott Thoreson <scott.thoreson@live.com>
2022-01-15 15:04:24 -0500

    Began working on the listedPlanet section

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 // Write your JavaScript code here!
 
-try{
-    const helper = require("./scriptHelper");
-}catch(e){
-    //do nothing
-}
+// try{
+//     const helper = require("./scriptHelper");
+// }catch(e){
+//     //do nothing
+// }
 
 window.addEventListener("load", function() {
 
-//    let listedPlanets;
-//    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-//    let listedPlanetsResponse = myFetch();
-//    listedPlanetsResponse.then(function (json) {
-//        console.log(json);
-// //    }).then(function () {
-// //        console.log(listedPlanets);
-//        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-//    });
+    let listedPlanets;
+    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function (json) {
+        listedPlanets = json;
+        let planetSelected = pickPlanet(listedPlanets);
+        helper.addDestinationInfo(document,planetSelected.name,planetSelected);
+    //    console.log(json);
+        
+//    }).then(function () {
+//        console.log(listedPlanets);
+       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    });
     const faultyItemsElement = document.getElementById("faultyItems");
     faultyItemsElement.style.visibility = "hidden";
-    let form = document.getElementById("userIn");//.querySelector("form");
+    let form = document.querySelector("form");//.getElementsByClassName("formField");//.querySelector("form");
     const propagation = true;
-    form.querySelector("Div").addEventListener("submit", function(event){
+    form.addEventListener("submit", function(event){
         let pilotNameInput = document.querySelector("input[id=pName]");
         let copilotNameInput = document.querySelector("input[id=cName]");
         let fuelLevelInput = document.querySelector("input[id=fuelLvl]");
         let cargoMassInput = document.querySelector("input[id=cargoKg]");
-        formSubmission(document.getElementsByTagName("Html"),faultyItemsElement, pilotNameInput,copilotNameInput,fuelLevelInput,cargoMassInput);
+        helper.formSubmission(document,faultyItemsElement, pilotNameInput,copilotNameInput,fuelLevelInput,cargoMassInput);
         event.preventDefault();
         // faultyItemsElement.style.visibility = "visible";
     });
